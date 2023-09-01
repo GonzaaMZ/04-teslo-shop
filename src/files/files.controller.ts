6172aa370c3ee4a92175ce4197bc0450e11f82ac
GonzaaMZ <gonzaloamaya1999@gmail.com>
2023-09-01 12:11:13 -0300

    seccion carga de archivos terminada

---
+++ src/files/files.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+  Get,
+  Param,
+  Res,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { FilesService } from './files.service';
+import { fileNamer, fileFilter } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProduct(imageName);
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: {}
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('File is empty');
+    }
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
+    return {
+      secureUrl,
+    };
+  }
+}
---
+++ src/files/helpers/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export { fileFilter } from './fileFilter';
+export { fileNamer } from './fileNamer';
